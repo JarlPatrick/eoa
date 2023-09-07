0ea4247f732810c83f3215e37b771bf23750b102
MartinRahe <martin.r.rahe@gmail.com>
2023-09-07 04:20:23 +0300

    changed sql queries: for => 4

--- eoa.ee/code/tulemus_by_id.php
+++ eoa.ee/code/tulemus_by_id.php
@@ -1,10 +1,10 @@
 <?php namespace tul_by_id {
 
 function get_title($conn, $id): string {
-    $sql = "SELECT subcontest.id AS id, subcontest.name AS subcontest_name,
-    contest.id AS contest_id, contest.name AS contest_name, tasks_link FROM subcontest
-    LEFT JOIN contest ON subcontest.contest_id = contest.id
-    WHERE subcontest.id=".$id.";";
+    $sql = "SELECT subcontest.name AS subcontest_name,
+        contest.name AS contest_name, tasks_link FROM subcontest
+        LEFT JOIN contest ON subcontest.contest_id = contest.id
+        WHERE subcontest.id=".$id.";";
 	$result = $conn->query($sql);
 	if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
@@ -20,93 +20,119 @@ function get_title($conn, $id): string {
 	return $out;
 }
 
-function name_by_id($conn, $table_name): array {
-	$sql = "SELECT id, name FROM ".$table_name.";";
-	$result = $conn->query($sql);
-	$nimed = array();
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			$nimed[$row["id"]] = $row["name"];
-		}
-	}
-	return $nimed;
+function get_columns($conn, $id): array {
+    $sql = "SELECT id, name FROM subcontest_column
+        WHERE subcontest_id=".$id.";";
+    $result = $conn->query($sql);
+    $out = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $out[] = $row;
+        }
+    }
+    return $out;
 }
 
-function get_results($conn,$ids): array {
-	$entrys = array();
-	foreach ($ids as &$id){
-		$entry = array();
-		$sql = "SELECT * FROM contestant_field where task_id=".$id.";";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-				$entry[$row["contestant_id"]] = $row["entry"];
-			}
-		}
-		$entrys[$id] = $entry;
-	}
-	return $entrys;
+function get_contestant_info($conn, $id): array {
+    $sql = "SELECT contestant.id AS contestant_id, placement, person_id,
+        person.name AS person_name, age_group.name AS age_group_name,
+        school_id, school.name AS school_name, mentor_id,
+        mentorPerson.name AS mentor_name FROM contestant
+        LEFT JOIN person ON contestant.person_id = person.id
+        LEFT JOIN age_group ON contestant.age_group_id = age_group.id
+        LEFT JOIN school ON contestant.school_id = school.id
+        LEFT JOIN mentor ON contestant.id = mentor.contestant_id
+        LEFT JOIN person mentorPerson ON mentor.mentor_id = mentorPerson.id
+        WHERE subcontest_id=".$id."
+        ORDER BY ISNULL(placement), placement;";
+    $result = $conn->query($sql);
+    $out = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $out[] = $row;
+        }
+    }
+    return $out;
 }
 
-function get_mentor($conn, $id): array {
-	$mentors = array();
-	$sql = "SELECT mentor_id FROM mentor where contestant_id=".$id.";";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			array_push($mentors, $row["mentor_id"]);
-		}
-	}
-	return $mentors;
-	
+function get_results($conn, $ids): array {
+	$entries = array();
+    $idsString = implode(", ", $ids);
+    $sql = "SELECT * FROM contestant_field
+        WHERE task_id IN (".$idsString.")
+        ORDER BY FIELD(task_id, ".$idsString.");";
+    $result = $conn->query($sql);
+    $resultObjects = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $resultObjects[] = $row;
+        }
+    }
+    $oldID = "";
+    $entry = array();
+    foreach ($resultObjects as $resultObject) {
+        $id = $resultObject["task_id"];
+        if ($id != $oldID) {
+            if ($oldID != "") {
+                $entries[$oldID] = $entry;
+            }
+            $entry = array();
+            $oldID = $id;
+        }
+        $entry[$resultObject["contestant_id"]] = $resultObject["entry"];
+    }
+    $entries[$oldID] = $entry;
+    return $entries;
 }
 
 function tulemus($conn, $id): string {
 	
-	$nimed = name_by_id($conn,"person");
-	$vanus = name_by_id($conn,"age_group");
-	$kool = name_by_id($conn,"school");
-	
 	$out = get_title($conn, $id);
-	
 	
 	$out.="<tr><th>Koht</th>";
 	$out.="<th>Nimi</th><th>Klass</th><th>Kool</th><th>Juhendaja</th>";
-	$tasksql = "SELECT * FROM subcontest_column where subcontest_id=".$id.";";
-	$taskresult = $conn->query($tasksql);
-	$nrTask = array();
-	if ($taskresult->num_rows > 0) {
-			while($row = $taskresult->fetch_assoc()) {
-					$out .= "<th>".$row["name"]."</th>";
-					array_push($nrTask, $row["id"]);
-			}
-	}
+	$columns = get_columns($conn, $id);
+	$taskIDs = array();
+    foreach ($columns as $column) {
+        $out .= "<th>".$column["name"]."</th>";
+        $taskIDs[] = $column["id"];
+    }
+    $out.="</tr>";
 	
-	$results = get_results($conn, $nrTask);
-	
-	$out.="</tr>";
-	
-	
-	$CIsql = "SELECT * FROM contestant where subcontest_id=".$id." order by isnull(placement), placement;";
-	$CIresult = $conn->query($CIsql);
-	if ($CIresult->num_rows > 0) {
-			// output data of each row
-			while($row = $CIresult->fetch_assoc()) {
-					$out .= "<tr class='item'><td>".$row["placement"]."</td><td><a href='?name_id=".$row["person_id"]."'>".$nimed[$row["person_id"]]."</a></td>";
-					$out .= "<td>".$vanus[$row["age_group_id"]]."</td><td><a href='?school_id=".$row["school_id"]."'>".$kool[$row["school_id"]]."</a></td>";
-					$mentor = get_mentor($conn, $row["id"]);
-					$out .="<td>";
-					foreach ($mentor as &$men){
-						if(array_search($men, $mentor) > 0){$out .=" / ";}
-						$out .="<a href='?name_id=".$men."'>";
-						$out .=$nimed[$men]."</a>";
-					}$out .="</td>";
-					foreach ($nrTask as &$ent){
-						$out .="<td>".$results[$ent][$row["id"]]."</td>";
-					}
-					$out .= "</tr>";
-			}
-	}
+	$results = get_results($conn, $taskIDs);
+
+    $contestantInfoObjects = get_contestant_info($conn, $id);
+    $mentors = array();
+    foreach ($contestantInfoObjects as $index => $contestantInfoObject) {
+        $addRow = true;
+        if (isset($contestantInfoObjects[$index+1])) {
+            if ($contestantInfoObjects[$index+1]["person_id"] == $contestantInfoObject["person_id"]) {
+                $addRow = false;
+            }
+        }
+        if ($addRow) {
+            $out .= "<tr class='item'><td>".$contestantInfoObject["placement"]."</td><td><a href='?name_id=".$contestantInfoObject["person_id"]."'>".$contestantInfoObject["person_name"]."</a></td>";
+            $out .= "<td>".$contestantInfoObject["age_group_name"]."</td><td><a href='?school_id=".$contestantInfoObject["school_id"]."'>".$contestantInfoObject["school_name"]."</a></td>";
+            $mentors[] = array("id" => $contestantInfoObject["mentor_id"], "name" => $contestantInfoObject["mentor_name"]);
+            $out .="<td>";
+            foreach ($mentors as $mentor){
+                if (array_search($mentor, $mentors) > 0){$out .=" / ";}
+                if (!is_null($mentor["id"])) {
+                    $out .="<a href='?name_id=".$mentor["id"]."'>";
+                    $out .=$mentor["name"]."</a>";
+                }
+            }
+            $out .="</td>";
+            $mentors = array();
+            foreach ($taskIDs as $taskID){
+                $out .="<td>".$results[$taskID][$contestantInfoObject["contestant_id"]]."</td>";
+            }
+        } else {
+            $mentors[] = array("id" => $contestantInfoObject["mentor_id"], "name" => $contestantInfoObject["mentor_name"]);
+        }
+
+    }
+
 	$out .= "</table></div></center>";
 	
 	return $out;
